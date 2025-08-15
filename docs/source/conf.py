dc27e15055ff2fac3b55bc62e14110cb813a8fcf
vovaman <bd_postbox1@mail.ru>
2025-08-15 16:02:33 +0300

    #51 исправлены зависимости от других пакетов

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'prs-connector-core'
 copyright = '2025, ООО МПК'
 author = 'ООО МПК'
-release = '0.3.1'
+release = '0.3.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
